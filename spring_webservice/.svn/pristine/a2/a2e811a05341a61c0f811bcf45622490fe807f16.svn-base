9f238608cefa00faf81635f272371b60cb4e1239
yenairong <1039288191@qq.com>
2018-10-22 22:32:16 +0800

    最新加入

---
+++ spring_webservice/.svn/pristine/a2/a2e811a05341a61c0f811bcf45622490fe807f16.svn-base
@@ -0,0 +1,12 @@
+package spring_webservice;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+//https://www.cnblogs.com/moon521/p/5564504.html?tdsourcetag=s_pcqq_aiomsg
+
+//http://localhost:8080/yourProgramName/webservice/Greeting?wsdl
+@WebService
+public interface Greeting {
+	public String greeting(@WebParam(name = "username") String userName);
+}
---
+++ spring_webservice/src/main/java/spring_webservice/GreetingImpl.java
@@ -0,0 +1,15 @@
+package spring_webservice;
+
+import java.util.Calendar;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface="com.moon.cxfWebservice.server.Greeting")
+public class GreetingImpl implements Greeting{
+
+    public String greeting(String userName) {
+        return "Hello " + userName + ", currentTime is "
+                + Calendar.getInstance().getTime();
+    }
+
+}
